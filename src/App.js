b28715aab78df1075e269a64e028bf817adf0de8
Rabiul Islam <rabibd.it@gmail.com>
2021-09-18 21:24:24 +0600

    Country Added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.eu/rest/v2/all')
+    .then(res => res.json())
+    .then(data => setCountries(data))
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h2>MY FIRST REACT REST COUNTRIES</h2>
+        <div className="country">
+          {
+            countries.map((country, index) => <Country key={index} name={country.name} flag={country.flag} timezone={country.timezones}></Country>)
+          }
+        </div>
       </header>
+    </div>
+  );
+}
+
+function Country(props){
+  const countryStyle = {
+    backgroundColor: 'white',
+    borderRadius: '20px 20px 0 0',
+    color:'black'
+  }
+  return (
+    <div style={countryStyle}>
+        <h3>{props.name}</h3>
+        <img height="200" width="100%" src={props.flag} alt={props.name} />
+        <p>TimeZone: {props.timezone[0]}</p>
     </div>
   );
 }
